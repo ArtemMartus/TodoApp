5440a74a003b021c7f0604d6434a1bff57ed3732
Artem <artemmartus2012@gmail.com>
2019-08-29 13:27:10 +0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Artem Martus on 8/29/19.
+//  Copyright © 2019 Artem Martus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
